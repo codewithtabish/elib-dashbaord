b8d3504ba9c49076fd1383ae386902d1bef107ea
DESKTOP-NPUVJIQ\Mark 1 <tabish169000@gmail.com>
2024-05-06 05:11:03 -0700

    vite app basic setup with shadecn

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h2 className="text-3xl text-red-700">
+        THIS IS THE ROOT MAIN Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Labore, ipsum natus reiciendis nostrum ab accusamus officia libero
+        iusto cupiditate, obcaecati repellendus blanditiis odio assumenda
+        quaerat est impedit facilis maiores? Temporibus.
+      </h2>
+    </>
+  );
+}
+
+export default App;
